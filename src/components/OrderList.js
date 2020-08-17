6da6665788dc19089807c66e157a81ca29b85504
blenky36 <philblenk@hotmail.co.uk>
2020-08-17 14:02:57 +0100

    table styling

--- src/components/OrderList.js
+++ src/components/OrderList.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { getOrderOutputSweetPacks } from '../_selectors/order.selectors';
+import styled from 'styled-components';
+
+const Table = styled.table`
+    border-collapse: collapse;
+    font-size: 80%;
+    font-weight: bold;
+`;
+
+const TableHead = styled.thead`
+    border-radius: 4px;
+    border: 1px solid #00C851;    
+    background-color: #00C851;
+`;
+
+const TableData = styled.td`
+    padding: 15px;
+`;
 
 const OrderList = () => {
-    // const orderOutputSweetPacks = { 500: 1, 3000: 2 };
-    const orderOutputSweetPacks = {  };
+    const orderOutputSweetPacks = { 500: 1, 3000: 2 };
 
     const isEmpty = (obj) => {
         for (var prop in obj) {
@@ -20,15 +36,15 @@ const OrderList = () => {
         if (!orderOutputSweetPacks || isEmpty(orderOutputSweetPacks)) {
             sweetPackList.push(
                 <tr>
-                    <td colSpan="2"><i>Please order some sweets.</i></td>
+                    <TableData colSpan="2"><i>Please order some sweets.</i></TableData>
                 </tr>
             )
         } else {
             for (const sweetPack in orderOutputSweetPacks) {
                 sweetPackList.push(
                     <tr>
-                        <td>{sweetPack}</td>
-                        <td>{orderOutputSweetPacks[sweetPack]}</td>
+                        <TableData>{sweetPack}</TableData>
+                        <TableData>{orderOutputSweetPacks[sweetPack]}</TableData>
                     </tr>
                 )
             }
@@ -37,15 +53,15 @@ const OrderList = () => {
     }
 
     return (
-        <table>
-            <thead>
-                <td>Sweet Pack Size</td>
-                <td>Quantity</td>
-            </thead>
+        <Table>
+            <TableHead>
+                <TableData>Sweet Pack Size</TableData>
+                <TableData>Quantity</TableData>
+            </TableHead>
             <tbody>
                 {getOrderList()}
             </tbody>
-        </table>
+        </Table>
     )
 }
 
